400a7c138ec7c50760c4198653b576dc2e5bf8e3
KodingNYoung <abiodunadebambo44@gmail.com>
2025-08-27 04:04:52 +0100

    chore(): optimizing meta tags for facebook and whatsapp image

--- src/components/shared/molecules/MetaProps.tsx
+++ src/components/shared/molecules/MetaProps.tsx
@@ -18,8 +18,10 @@ const MetaProps: FC<Props> = ({title, description}) => {
         <meta property="og:description" content={description} />
 
         <meta name="image" content="/social-image.jpeg" />
-        <meta property="og:image" content="/social-image.jpeg" />
+        <meta property="og:image" content="https://www.eusate.com/social-image.jpeg" />
         <meta property="og:image:alt" content="eusate logo" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
