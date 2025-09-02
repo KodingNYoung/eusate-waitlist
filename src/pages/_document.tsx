1db6bd779678cf049e35e0985207da1aee273300
KodingNYoung <abiodunadebambo44@gmail.com>
2025-09-03 00:50:45 +0100

    chore(): fixed google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,19 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
+      <Head>
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-M7Y5L4KS2D"
         />
-        <script id="g-analytics">
-          {`window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-M7Y5L4KS2D');`}
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-M7Y5L4KS2D');
+              console.log('Google Analytics script executed');
+            `,
+          }}
+        />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
