7abc871bc8071fb380832ddd4997c1e5e1c95a42
KodingNYoung <abiodunadebambo44@gmail.com>
2026-01-27 22:30:36 +0100

    chore(): reverted Eusate widget removal from page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,29 @@
 import { plusJakartaSans } from "@/assets/fonts";
 import "@/styles/globals.css";
 import { cls } from "@/utils/helpers";
-// import Eusate from "@eusate/messenger-sdk";
+import Eusate from "@eusate/messenger-sdk";
 import type { AppProps } from "next/app";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { Slide, ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // useEffect(() => {
-  //   // Initialize SDK once when app mounts
-  //   Eusate.init({
-  //     apiKey: process.env.NEXT_PUBLIC_EUSATE_API_KEY!,
-  //     onReady: () => {
-  //       console.log("✅ Eusate chat is ready");
-  //     },
-  //     onError: (error) => {
-  //       console.error("❌ Eusate initialization failed:", error);
-  //     },
-  //   });
+  useEffect(() => {
+    // Initialize SDK once when app mounts
+    Eusate.init({
+      apiKey: process.env.NEXT_PUBLIC_EUSATE_API_KEY!,
+      onReady: () => {
+        console.log("✅ Eusate chat is ready");
+      },
+      onError: (error) => {
+        console.error("❌ Eusate initialization failed:", error);
+      },
+    });
 
-  //   // Cleanup when app unmounts
-  //   return () => {
-  //     Eusate.destroy();
-  //   };
-  // }, []); // Empty dependency array = runs once
+    // Cleanup when app unmounts
+    return () => {
+      Eusate.destroy();
+    };
+  }, []); // Empty dependency array = runs once
   return (
     <main className={cls("min-h-screen", plusJakartaSans.className)}>
       <Component {...pageProps} />
